495a246a2cc612b8784df0450b9e9126fe21e697
委员长 <xiaoqiang6@vip.qq.com>
2022-04-30 10:56:39 +0800

    增加远程线程注入功能
    通过kernel32.dll模块中的LoadLibraryA实现远程线程注入

--- DllInjector/RemoteThreadInject.cpp
+++ DllInjector/RemoteThreadInject.cpp
@@ -9,8 +9,40 @@ CRemoteThreadInject::~CRemoteThreadInject()
 {
 }
 
+void TcharToChar(const TCHAR* tchar, char* _char)
+{
+    int iLength = WideCharToMultiByte(CP_ACP, 0, tchar, -1, NULL, 0, NULL, NULL);
+    WideCharToMultiByte(CP_ACP, 0, tchar, -1, _char, iLength, NULL, NULL);
+}
+
 bool CRemoteThreadInject::InjectorDLL(TCHAR* szPath, DWORD dwPid)
 {
-    MessageBox(0, 0, L"Ô¶³ÌÏß³Ì", 0);
-    return false;
+    HANDLE hProcess = GetProcessHandle(dwPid);
+    if (hProcess == NULL)
+    {
+        return false;
+    }
+
+    LPVOID lpAdd = NULL;
+    lpAdd = VirtualAllocEx(hProcess, NULL, MAX_PATH, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    if (lpAdd == NULL)
+    {
+        return false;
+    }
+    char szTmpPath[MAX_PATH] = { 0 };
+    TcharToChar(szPath, szTmpPath);
+    if (!WriteProcessMemory(hProcess, lpAdd, szTmpPath, strlen(szTmpPath) + 1, NULL))
+    {
+        VirtualFreeEx(hProcess, lpAdd, MAX_PATH, MEM_RELEASE);
+        return false;
+    }
+
+    HMODULE hModule = GetModuleHandle(L"kernel32.dll");
+
+    HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)GetProcAddress(hModule, "LoadLibraryA"), lpAdd, 0, NULL);
+    WaitForSingleObject(hThread, INFINITE);
+    CloseHandle(hThread);
+    VirtualFreeEx(hProcess, lpAdd, MAX_PATH, MEM_RELEASE);
+
+    return true;
 }
